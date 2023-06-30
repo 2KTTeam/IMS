e0688cbf916ea6106abbee359c30a7c37f2e63b3
fudosman <fudosman@gmail.com>
2023-06-30 02:14:38 +0100

    secrete logger added

--- src/utils/uuid.util.js
+++ src/utils/uuid.util.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid');
 
-const cacheFilePath = path.join(__dirname, '../registrationIdCache.txt');
+const cacheFilePath = path.join(__dirname, '../.registrationIdCache.txt');
 
 const readCachedIDs = () => {
   try {
