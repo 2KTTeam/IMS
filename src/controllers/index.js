431cee23eb11889339ccabdffaad40ae844d0005
Toby Chidi <111127651+tobychidi@users.noreply.github.com>
2023-07-05 15:44:17 +0100

    CRUD Routes for projects and users

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,11 @@
 const auth = require("./auth.controller");
-const profile = require("./profile.controller");
+const user = require("./user.controller");
 const project = require("./project.controller");
 const public = require("./public.controller");
 
 module.exports = {
    auth,
+   user,
    project,
    public,
-   profile,
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 
-const profile = require('./profile.route');
+const profile = require("./profile.route");
 const auth = require("./auth.route");
 const project = require("./project.route");
 const public = require("./public.route");
+const user = require("./user.route");
 
 app.use("/auth", auth);
+app.use("/users", user);
 app.use("/projects", project);
 app.use("/public", public);
-app.use('/profile', profile);
 
 module.exports = app;
