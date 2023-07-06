097c9583cfe7224b9d8a6666a2f75d9a155b0fc9
Toby Chidi <111127651+tobychidi@users.noreply.github.com>
2023-07-06 13:09:27 +0100

    final update from tobychidi

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,119 +1,173 @@
 const { StatusCodes } = require("http-status-codes");
 const { auth } = require("../services");
-const {sendWelcomeEmail, welcomeUser } = require('../email');
-const {managerData} = require('../configs');
+const { sendWelcomeEmail, welcomeUser } = require("../email");
+const { managerData } = require("../configs");
 const { userService } = require("../models");
 
-
 const registerManager = async (managerData) => {
-  try {
-    const { firstname, lastname, email, role, password, gender, dateOfBirth, phoneNumber, address } = managerData;
+   try {
+      const {
+         firstname,
+         lastname,
+         email,
+         role,
+         password,
+         gender,
+         dateOfBirth,
+         phoneNumber,
+         address,
+      } = managerData;
 
-    const newUser = {
-      firstname, lastname, email, role, password, gender, dateOfBirth, phoneNumber, address
-    }
+      const newUser = {
+         firstname,
+         lastname,
+         email,
+         role,
+         password,
+         gender,
+         dateOfBirth,
+         phoneNumber,
+         address,
+      };
 
-    await auth.register(newUser);
-    console.log('Manager Successfully Seeded into Database')
-
-  } catch (error) {
-    console.log('a manager now exists in the database')
-  }
+      await auth.register(newUser);
+      console.log("Manager Successfully Seeded into Database");
+   } catch (error) {
+      console.log("a manager now exists in the database");
+   }
 };
 
 registerManager(managerData);
 
 const registerAdmin = async (req, res) => {
-  try {
-    const { firstname, lastname, email, password, gender, dateOfBirth, phoneNumber, address } = req.body;
+   try {
+      const {
+         firstname,
+         lastname,
+         email,
+         password,
+         gender,
+         dateOfBirth,
+         phoneNumber,
+         address,
+      } = req.body;
 
-    const currentUser =  userService.queryOne({email});
+      const currentUser = userService.queryOne({ email });
 
-    if(currentUser) return res.status(StatusCodes.CREATED).json({message: 'User with email already exists', status: false})
+      if (currentUser)
+         return res
+            .status(StatusCodes.CREATED)
+            .json({ message: "User with email already exists", status: false });
 
-    const newUser = {
-      firstname, lastname, email, role: 'admin', password, gender, dateOfBirth, phoneNumber, address
-    }
+      const newUser = {
+         firstname,
+         lastname,
+         email,
+         role: "admin",
+         password,
+         gender,
+         dateOfBirth,
+         phoneNumber,
+         address,
+      };
 
-    const user = await auth.register(newUser);
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: 'Admin not registered!'
+      const user = await auth.register(newUser);
+      if (!user) {
+         return res.status(400).json({
+            success: false,
+            message: "Admin not registered!",
+         });
+      }
+
+      sendWelcomeEmail(email, firstname, lastname, email, password);
+
+      // Return the saved User object
+      return res.status(StatusCodes.CREATED).json({
+         success: true,
+         user,
       });
-    }
-
-    sendWelcomeEmail(email, firstname, lastname,email, password)
-
-    // Return the saved User object
-    return res.status(StatusCodes.CREATED).json({
-      success: true,
-      user
-    });
-  } catch (error) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      error: `Error creating Admin: ${error.message}`
-    });
-  }
-}
-
-const registerUser = async (req, res) => {
-  try {
-    const { firstname, lastname, email, password, gender, dateOfBirth, phoneNumber, address } = req.body;
-    
-    const currentUser =  userService.queryOne({email});
-
-    if(currentUser) return res.status(StatusCodes.CREATED).json({message: 'User with email already exists', status: false})
-
-    const newUser = {
-      firstname, lastname, email, role: 'user', password, gender, dateOfBirth, phoneNumber, address
-    }
-
-    const user = await auth.register(newUser);
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: 'Admin not registered!'
+   } catch (error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+         success: false,
+         error: `Error creating Admin: ${error.message}`,
       });
-    }
-    welcomeUser(email, firstname, lastname,email, password, user.verification_token)
-    // Return the saved User object
-    return res.status(StatusCodes.CREATED).json({
-      success: true,
-      user
-    });
-  } catch (error) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      error: `Error creating Admin: ${error.message}`
-    });
-  }
-}
-
-const login = async function (req, res) {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
-    const token = await auth.login(email, password);
-
-    return res.status(StatusCodes.OK).json({
-      status: "success",
-      token
-    })
-  } catch (error) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      error: `Error signing in: ${error.message}`,
-    });
-  }
+   }
 };
 
+const registerUser = async (req, res) => {
+   try {
+      const {
+         firstname,
+         lastname,
+         email,
+         password,
+         gender,
+         dateOfBirth,
+         phoneNumber,
+         address,
+      } = req.body;
 
+      const currentUser = userService.queryOne({ email });
+
+      if (currentUser)
+         return res
+            .status(StatusCodes.CREATED)
+            .json({ message: "User with email already exists", status: false });
+
+      const newUser = {
+         firstname,
+         lastname,
+         email,
+         role: "user",
+         password,
+         gender,
+         dateOfBirth,
+         phoneNumber,
+         address,
+      };
+
+      const user = await auth.register(newUser);
+      if (!user) {
+         return res.status(400).json({
+            success: false,
+            message: "Admin not registered!",
+         });
+      }
+      welcomeUser(email, firstname, lastname, email, password, user.verificaiton_token);
+      // Return the saved User object
+      return res.status(StatusCodes.CREATED).json({
+         success: true,
+         user,
+      });
+   } catch (error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+         success: false,
+         error: `Error creating Admin: ${error.message}`,
+      });
+   }
+};
+
+const login = async function (req, res) {
+   try {
+      const email = req.body.email;
+      const password = req.body.password;
+      const token = await auth.login(email, password);
+
+      return res.status(StatusCodes.OK).json({
+         status: "success",
+         token,
+      });
+   } catch (error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+         success: false,
+         error: `Error signing in: ${error.message}`,
+      });
+   }
+};
 
 module.exports = {
-  registerManager,
-  registerAdmin,
-  registerUser,
-  login,
+   registerManager,
+   registerAdmin,
+   registerUser,
+   login,
 };
